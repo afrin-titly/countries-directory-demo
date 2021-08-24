f95f0677c4c261e78d455cec728d551ff72fd8c6
lm-afrin <titlee42@gmail.com>
2021-08-24 22:00:23 +0900

    initializing tailwind and added navbar

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1> Country Directory App</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
